a652b240031d01ac981b850254a3c8effe1737a6
Raul_ICD <143694129+RuloNX@users.noreply.github.com>
2024-04-25 19:30:04 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -93,8 +93,19 @@ void eraseMap(HashMap * map,  char * key) {
 
 }
 
-Pair * searchMap(HashMap * map,  char * key) {   
+Pair * searchMap(HashMap * map, char * key) {
+    if (map == NULL || key == NULL) return NULL;
 
+    long index = hash(key, map->capacity);
+    Pair *current = map->buckets[index];
+
+    while (current != NULL) {
+        if (is_equal(current->key, key)) {
+            map->current = index;
+            return current;
+        }
+        current = current->next; // Move to the next Pair
+    }
 
     return NULL;
 }
