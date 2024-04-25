57c8e75fd44adff3188a7d2fa853295dff00dc93
Raul_ICD <143694129+RuloNX@users.noreply.github.com>
2024-04-25 19:13:47 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -51,25 +51,28 @@ void enlarge(HashMap * map) {
 
 
 HashMap * createMap(long capacity) {
+    HashMap * map = (HashMap *)malloc(sizeof(HashMap));
+    if (map == NULL) {
+        return NULL;
+    }
 
-  HashMap *map = (HashMap*) malloc(sizeof(HashMap));
+    map->buckets = (Pair **)malloc(capacity * sizeof(Pair *));
+    if (map->buckets == NULL) {
+        free(map);
+        return NULL;
+    }
 
-  if(map==NULL){
-    return NULL;
-  }
+    map->size = 0;
+    map->capacity = capacity;
+    map->current = -1;
 
-  map->buckets = (Pair**) malloc(capacity*sizeof(Pair*));
-  if (map->buckets == NULL) {
-    free(map);
-    return NULL;
-  }
+    for (long i = 0; i < capacity; i++) {
+        map->buckets[i] = NULL;
+    }
 
-  map->size = 0;
-  map->capacity = capacity;
-  map->current = -1;
-
-  return NULL;
+    return map;
 }
+
 
 void eraseMap(HashMap * map,  char * key) {    
 
